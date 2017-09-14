334810c8a8a6162f77fed8653f970d03f07a4f9a
Daniel Ramos <daniel.ramosg@gmail.com>
2017-09-14 18:57:30 +0100

    Spanish translation.

--- js/translations.js
+++ js/translations.js
@@ -3,200 +3,269 @@ var dict = {
   "Information": {
     en: "Information",
     pt: "Informação",
-    fr: "Information"
+    fr: "Information",
+    es: "Información",
+    ca: "Informació"
   },
   
   "Start": {
     en: "Start",
     pt: "Início",
-    fr: "Démarrer"
+    fr: "Démarrer",
+    es: "Inicio",
+    ca: "Inici"
   },
 
   "About": {
     en: "About",
     pt: "Acerca de",
-    fr: "À propos"
+    fr: "À propos",
+    es: "Acerca de",
+    ca: "Sobre"
   },
   
     "Geodesic": {
     en: "Geodesic",
     pt: "Geodésica",
-    fr: "Géodésique"
+    fr: "Géodésique",
+    es: "Geodésica",
+    ca: "Geodèsica"
   },
 
   "Loxodrome": {
     en: "Loxodrome",
     pt: "Loxodrómia",
-    fr: "Loxodromie"
+    fr: "Loxodromie",
+    es: "Loxodroma",
+    ca: "Loxodroma"
   },
 
   "Projections": {
     en: "Projections",
     pt: "Projeções",
-    fr: "Projections"
+    fr: "Projections",
+    es: "Proyecciones",
+    ca: "Projeccions"
   },
   
   "Tools": {
     en: "Tools",
     pt: "Ferramentas",
-    fr: "Outils"
+    fr: "Outils",
+    es: "Herramientas",
+    ca: "Eines"
   },
   
   "View": {
     en: "View",
     pt: "Vista",
-    fr: "Vue"
+    fr: "Vue",
+    es: "Vista",
+    ca: "Vista"
   },
   
   "Tissot's indicatrix": {
     en: "Tissot's indicatrix",
     pt: "Indicatriz de Tissot",
-    fr: "Indicatrice de Tissot"
+    fr: "Indicatrice de Tissot",
+    es: "Indicatriz de Tissot",
+    ca: "Indicatriu de Tissot"
   },
   
   "Clear": {
     en: "Clear",
     pt: "Limpar",
-    fr: "Effacer"
+    fr: "Effacer",
+    es: "Borrar",
+    ca: "Esborrar"
   },
     
   "Undo": {
     en: "Undo",
     pt: "Anular",
-    fr: "Annuler"
+    fr: "Annuler",
+    es: "Deshacer",
+    ca: "Desfer"
   },
   
   "Sample": {
     en: "Sample",
     pt: "Amostra",
-    fr: "Échantillon"
+    fr: "Échantillon",
+    es: "Muestra",
+    ca: "Mostra"
   },
   
   "Goniometer": {
     en: "Goniometer",
     pt: "Goniómetro",
-    fr: "Goniomètre"
+    fr: "Goniomètre",
+    es: "Goniómetro",
+    ca: "Goniòmetre"
   },
   
   "Radius": {
     en: "Radius",
     pt: "Raio",
-    fr: "Rayon"
+    fr: "Rayon",
+    es: "Radio",
+    ca: "Radi"
   },
 
   "Extend": {
     en: "Extend",
     pt: "Estender",
-    fr: "Étendre"
+    fr: "Étendre",
+    es: "Extender",
+    ca: "Estendre"
   },
 
   "Aspect": {
     en: "Aspect",
     pt: "Aspecto",
-    fr: "Aspect"
+    fr: "Aspect",
+    es: "Aspecto",
+    ca: "Aspecte"
   },
 
   "Fix aspect": {
     en: "Fix aspect",
     pt: "Fixar aspecto",
-    fr: "Fixer l'aspect"
+    fr: "Fixer l'aspect",
+    es: "Fijar el aspecto",
+    ca: "Fixar l'aspecte"
   },
 
   "Equatorial": {
     en: "Equatorial",
     pt: "Equatorial",
-    fr: "Équatorial"
+    fr: "Équatorial",
+    es: "Ecuatorial",
+    ca: "Equatorial"
   },
 
   "Polar (North)": {
     en: "Polar (North)",
     pt: "Polar (Norte)",
-    fr: "Polaire (Nord)"
+    fr: "Polaire (Nord)",
+    es: "Polar (Norte)",
+    ca: "Polar (Nord)"
   },
 
   "Polar (South)": {
     en: "Polar (South)",
     pt: "Polar (Sul)",
-    fr: "Polaire (Sud)"
+    fr: "Polaire (Sud)",
+    es: "Polar (Sur)",
+    ca: "Polar (Sud)"
   },
 
   "Oblique (Your location)": {
     en: "Oblique (Your location)",
     pt: "Oblíquo (Sua localização)",
-    fr: "Oblique (Votre position)"
+    fr: "Oblique (Votre position)",
+    es: "Oblícuo (Tu posición)",
+    ca: "Oblic (Vostra posició)"
   },
 
   "Oblique (Other)": {
     en: "Oblique (Other)",
     pt: "Oblíquo (Outra)",
-    fr: "Oblique (Autre)"
+    fr: "Oblique (Autre)",
+    es: "Oblícuo (Otra)",
+    ca: "Oblic (Altra)"
   },
 
   "Terrain": {
     en: "Terrain",
     pt: "Terreno",
-    fr: "Terrain"
+    fr: "Terrain",
+    es: "Terreno",
+    ca: "Terreny"
   },
 
   "Country borders": {
     en: "Country borders",
     pt: "Fronteiras",
-    fr: "Frontières des pays"
+    fr: "Frontières des pays",
+    es: "Fronteras",
+    ca: "Fronteres"
   },
 
   "Graticule": {
     en: "Graticule",
     pt: "Rede geográfica",
-    fr: "Graticule"
+    fr: "Graticule",
+    es: "Gratícula",
+    ca: "Gratícula"
   },
 
-  "Sample": {
-    en: "Sample",
-    pt: "Amostra",
-    fr: "Échantillon"
-  },
 
 // Projection names
 
 "Plate Carrée": {
   	en: "Plate Carrée",
 	pt: "Cilíndrica Equidistante",
-	fr: "Plate Carrée", },
+	fr: "Plate Carrée",
+    es: "Plate Carrée",
+    ca: "Plate Carrée" 
+   },
 
 "Mercator": {
 	en: "Mercator",
 	pt: "Mercator", 
-	fr: "Mercator", },
+	fr: "Mercator",
+    es: "Mercator",
+    ca: "Mercator",
+    },
 
 "Gall-Peters": {
 	en: "Gall-Peters",
 	pt: "Cilíndrica Equivalente", 
-	fr: "Gall-Peters", },
+	fr: "Gall-Peters",
+    es: "Gall-Peters",
+    ca: "Gall-Peters",
+    },
 
 "Mollweide": {
 	en: "Mollweide",
 	pt: "Mollweide", 
-	fr: "Mollweide", },
+	fr: "Mollweide",
+    es: "Mollweide",
+    ca: "Mollweide"
+    },
 
 "Azimuthal Equidistant": {
 	en: "Azimuthal Equidistant",
 	pt: "Azimutal Equidistante", 
-	fr: "Postel", },
+	fr: "Postel",
+    es: "Azimutal Equidistante",
+    ca: "Azimutal Equidistant"
+    },
 	
 "Gnomonic": {
 	en: "Gnomonic",
 	pt: "Gnomónica", 
-	fr: "Gnomonique", },
+	fr: "Gnomonique",
+    es: "Gnomónica",
+    ca: "Gnomònica"
+    },
 
 "Stereographic": {
 	en: "Stereographic",
 	pt: "Estereográfica", 
-	fr: "Stéréographique", },
+	fr: "Stéréographique",
+    es: "Estereográfica",
+    ca: "Estereogràfica"
+    },
 
 "Orthographic": {
 	en: "Orthographic",
 	pt: "Ortográfica", 
-	fr: "Orthographique", },
+	fr: "Orthographique",
+    es: "Ortográfica",
+    ca: "Ortogràfica"
+    },
 
 
 
