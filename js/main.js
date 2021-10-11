1a323712e17b9f225e7521427c21a7e28ba13682
Daniel Ramos <daniel.ramosg@gmail.com>
2021-10-11 19:42:04 +0200

    Event handlers moved away from index.html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 
+Language = "en";
+
 CurrentLocation = [0,0];  //CurrentLocation = [-9.136500,38.707718];  //Lisbon
 rotateToCurrentLocation = [-CurrentLocation[0],-CurrentLocation[1]];
 
@@ -111,3 +113,60 @@ d3.json('./txt/ui-translations.json', function(error,data){
           .text(d => d.endonym)
           .on("click", d => {Language=d.isoCode; translateAll();} );
 });
+
+
+//Binding buttons to handlers
+
+// top_controls
+d3.select("#checktext").on("click",hideshowText);
+d3.select("#lang_menu").on("click",showLangMenu);
+
+d3.select("#button_start").on("click", function(){setText("info_map")} );
+d3.select("#radio_text_tool").on("click", updateText );
+d3.select("#radio_text_proj").on("click", updateText );
+d3.select("#button_about").on("click", function(){setText("about")} );
+
+// control_tools
+d3.select("#radio_tissot").on("click", mode_tissot );
+d3.select("#radio_geodesic").on("click", mode_geodesic );
+d3.select("#radio_loxodrome").on("click", mode_loxodrome );
+d3.select("#radio_aspect").on("click", mode_aspect );
+
+// control_view
+    // tissot_tools
+d3.select("#pop_ellipses").on("click", popEllipse );
+d3.select("#sample_ellipses").on("click", sampleEllipses );
+d3.select("#decorate_ellipses").on("click", function(){
+      d3.selectAll('.tissot_decoration')
+      .style('visibility', this.checked ? 'visible' : 'hidden');
+    } );
+d3.select("#ellipradius").on("change", ellipradiusChanged )
+    .attr("min",0).attr("max",20000).attr("step",100).attr("value",1000);
+
+    // geodesic_tools
+d3.select("#clear_geodesic").on("click", popGeodesic );
+d3.select("#geoextended").on("click", geodesic_draw );
+
+    // loxodrome_tools
+d3.select("#clear_loxodrome").on("click", popLoxodrome );
+d3.select("#loxoextended").on("click", loxodrome_draw );
+
+    // aspect_tools
+
+    // common_tools
+d3.select("#clear_all").on("click", function(){
+      clearEllipses(); clearGeodesic(); clearLoxodrome();
+    } );
+d3.select("#rastervisible").on("click", function(){
+      d3.select("canvas").style("visibility",this.checked? 'visible':'hidden');
+      d3.select(".land").style("visibility",this.checked? 'hidden':'visible');
+    } );
+d3.select("#countriesvisible").on("click", function(){
+      d3.select(".boundary").style("visibility",this.checked? 'visible':'hidden');
+    } );
+d3.select("#graticulevisible").on("click", function(){
+      d3.select(".graticule").style("visibility",this.checked? 'visible':'hidden');
+    } );
+
+    // control_projections
+    // //
